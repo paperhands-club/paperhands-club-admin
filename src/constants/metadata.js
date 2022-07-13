a708fbcba3ff1f4c0c17f496aa5b2bea32081372
tommy50101 <tommy50101@XC-PC10.LGroup.global>
2022-07-13 20:25:09 +0800

    first frame master

---
+++ src/constants/metadata.js
@@ -0,0 +1,13 @@
+import LINK from 'src/constants/urls';
+
+const metadata = {
+	APP_NAME: 'Boilerplate',
+	APP_DESCRIPTION: 'A boilerplate for Reactjs app using nextjs, redux, redux-thunk, antd, less',
+	FB_APP_ID: '',
+	IMG_SHARE: LINK.WEB_URL + '/icons/512x512.png',
+	KEY_WORDS: '',
+	WEB_URL: LINK.WEB_URL,
+	PRIMARY_COLOR: 'red',
+};
+
+export default metadata;
---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import Head from 'src/components/Head';
+
+import Home from 'src/containers/Home';
+
+const propTypes = {
+	// classes: PropTypes.object.isRequired,
+};
+
+const defaultProps = {
+	// classes: {},
+};
+
+const HomePage = (props) => {
+	// const { } = props;
+
+	return (
+		<>
+			<Head />
+			<Home />
+		</>
+	);
+};
+
+HomePage.propTypes = propTypes;
+
+HomePage.defaultProps = defaultProps;
+
+export default HomePage;
