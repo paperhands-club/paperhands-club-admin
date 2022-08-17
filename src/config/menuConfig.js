f8f0179d434ba57f3e470ca5916cc70a331f7731
tommy50101 <tommy50101@XC-PC10.LGroup.global>
2022-08-17 18:26:32 +0800

    refactor: 重構為新框架- js + react + antd 初始化

---
+++ src/config/menuConfig.js
@@ -0,0 +1,52 @@
+/**
+ * icon:菜单项图标
+ * roles:标明当前菜单项在何种角色下可以显示，如果不写此选项，表示该菜单项完全公开，在任何角色下都显示
+ */
+const menuList = [
+  {
+    title: "首页",
+    path: "/dashboard",
+    icon: "home",
+    roles:["admin","editor","guest"]
+  },
+  {
+    title: "权限测试",
+    path: "/permission",
+    icon: "lock",
+    children: [
+      {
+        title: "权限说明",
+        path: "/permission/explanation",
+        roles:["admin"]
+      },
+      {
+        title: "admin页面",
+        path: "/permission/adminPage",
+        roles:["admin"]
+      },
+      {
+        title: "guest页面",
+        path: "/permission/guestPage",
+        roles:["guest"]
+      },
+      {
+        title: "editor页面",
+        path: "/permission/editorPage",
+        roles:["editor"]
+      },
+    ],
+  },
+  {
+    title: "表格",
+    path: "/table",
+    icon: "table",
+    roles:["admin","editor"]
+  },
+  {
+    title: "用户管理",
+    path: "/user",
+    icon: "usergroup-add",
+    roles:["admin"]
+  },
+];
+export default menuList;
---
+++ src/config/routeMap.js
@@ -0,0 +1,21 @@
+import Loadable from 'react-loadable';
+import Loading from '@/components/Loading'
+const Dashboard = Loadable({loader: () => import(/*webpackChunkName:'Dashboard'*/'@/views/dashboard'),loading: Loading});
+const Explanation = Loadable({loader: () => import(/*webpackChunkName:'Explanation'*/'@/views/permission'),loading: Loading});
+const AdminPage = Loadable({loader: () => import(/*webpackChunkName:'AdminPage'*/'@/views/permission/adminPage'),loading: Loading});
+const GuestPage = Loadable({loader: () => import(/*webpackChunkName:'GuestPage'*/'@/views/permission/guestPage'),loading: Loading});
+const EditorPage = Loadable({loader: () => import(/*webpackChunkName:'EditorPage'*/'@/views/permission/editorPage'),loading: Loading});
+const Table = Loadable({loader: () => import(/*webpackChunkName:'Table'*/'@/views/table'),loading: Loading});
+const Error404 = Loadable({loader: () => import(/*webpackChunkName:'Error404'*/'@/views/error/404'),loading: Loading});
+const User = Loadable({loader: () => import(/*webpackChunkName:'User'*/'@/views/user'),loading: Loading});
+
+export default [
+  { path: "/dashboard", component: Dashboard, roles: ["admin","editor","guest"] },
+  { path: "/permission/explanation", component: Explanation, roles: ["admin"] },
+  { path: "/permission/adminPage", component: AdminPage, roles: ["admin"] },
+  { path: "/permission/guestPage", component: GuestPage, roles: ["guest"] },
+  { path: "/permission/editorPage", component: EditorPage, roles: ["editor"] },
+  { path: "/table", component: Table, roles: ["admin","editor"] },
+  { path: "/user", component: User, roles: ["admin"] },
+  { path: "/error/404", component: Error404 },
+];
